d90fc1657979a844512dc05eddb5651ad0347f65
Matt Deming <vballmatt44@gmail.com>
2022-06-08 15:05:01 -0600

    Fix: Add advanced routing for admin merchants

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'admin merchant show page' do
   it 'shows the name of the merchant' do
     @merch1 = Merchant.create!(name: 'Floopy Fopperations')
 
-    visit "/admin/merchants/#{@merch1.id}"
+    visit admin_merchant_path(@merch1)
 
     expect(page).to have_content('Name: Floopy Fopperations')
   end
@@ -12,17 +12,17 @@ RSpec.describe 'admin merchant show page' do
   it 'can update the merchant' do
     @merch1 = Merchant.create!(name: 'Floopy Fopperations')
 
-    visit "/admin/merchants/#{@merch1.id}"
+    visit admin_merchant_path(@merch1)
 
     click_link('Update Merchant')
 
-    expect(current_path).to eq("/admin/merchants/#{@merch1.id}/edit")
+    expect(current_path).to eq(edit_admin_merchant_path(@merch1))
 
     fill_in 'Name', with: 'Cherry Chidona'
 
     click_on('Save')
 
-    expect(current_path).to eq("/admin/merchants/#{@merch1.id}")
+    expect(current_path).to eq(admin_merchant_path(@merch1))
 
     expect(page).to have_content('Merchant has been successfully updated!')
 
