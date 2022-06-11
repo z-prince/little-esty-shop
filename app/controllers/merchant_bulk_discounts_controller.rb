fd9d33e353607f0a9f6871782e46142bde64db5d
Zachary P <zachary.s.prince@outlook.com>
2022-06-11 17:29:13 -0400

    Add create method

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -3,5 +3,21 @@ class MerchantBulkDiscountsController < ApplicationController
     @discounts = Merchant.find(params[:id]).bulk_discounts
   end
 
-  def new; end
+  def new
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def create
+    require 'pry'
+    binding.pry
+    merch = Merchant.find(params[:id])
+    merch.bulk_discount.create!(bulk_dis_params)
+    redirect_to "/merchants/#{merch.id}/bulk_discounts"
+  end
+
+  private
+
+  def bulk_dis_params
+    params.permit(:percentage_discount, :quantity)
+  end
 end
