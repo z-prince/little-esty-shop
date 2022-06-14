4e4cf500f9a43c19990f6b9688c72a2dd1a2e58c
Zachary P <zachary.s.prince@outlook.com>
2022-06-14 12:25:38 -0400

    Add holiday_data method

--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -3,4 +3,8 @@ class HolidayService
     response = HTTParty.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def holiday_data
+    get_url('https://date.nager.at/api/v3/PublicHolidays/2022/US')
+  end
 end
