0bad662f34384ce4adc445f908d92ee9d16f1aad
Zachary P <zachary.s.prince@outlook.com>
2022-06-14 12:42:32 -0400

    Add next_three_holidays method

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -6,4 +6,13 @@ class HolidayFacade
   def all_holidays
     service.holiday_data.map { |data| Holiday.new(data) }
   end
+
+  def next_three_holidays
+    the_three = []
+    upcoming = all_holidays.find_all { |holiday| holiday.date > Date.today.to_s }
+    3.times do
+      the_three << upcoming.shift
+    end
+    the_three
+  end
 end
